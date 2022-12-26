cc5bff4b846a8ce22ba4978e53a5926b23e99174
notlikethiss <notlikeethiss@gmail.com>
2022-12-26 15:24:55 +0300

    pre-final

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const taskInput = document.querySelector('#addTask')
 const taskList = document.querySelector('#taskList')
-
+const cleanButton = document.querySelector('#cleanToDo')
 
 const newTask = () => {
 	const task = document.createElement('li')
+	task.className = 'task'
 	task.innerText = `${taskInput.value}`
+	taskInput.value = ''
+
 	taskList.appendChild(task)
 }
 
@@ -12,4 +15,9 @@ taskInput.addEventListener('keypress', (e) => {
 	if(e.key === 'Enter'){
 		newTask()
 	}
+})
+
+cleanButton.addEventListener('click', () => {
+	taskList.innerHTML = ''
+	taskInput.value = ''
 })
