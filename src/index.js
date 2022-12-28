d19ef176491670e3335ceb4480e32e1038830507
notlikethiss <notlikeethiss@gmail.com>
2022-12-29 01:39:27 +0300

    fix tasks

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 const taskInput = document.querySelector('#addTask')
 const taskList = document.querySelector('#taskList')
 const cleanButton = document.querySelector('#cleanToDo')
+const body = document.querySelector('body')
+
+const tasks = []
+let finalTasks = tasks.split(', ')
+
+body.addEventListener('DOMContentLoaded', () => {
+	localStorage.getItem('tasks', finalTasks)
+	tasks.className = 'task'
+	tasks.innerText = `${taskInput.value}`
+	taskList.append(tasks)
+})
 
 const newTask = () => {
 	const task = document.createElement('li')
+	
 	task.className = 'task'
 	task.innerText = `${taskInput.value}`
-	taskInput.value = ''
-
 	taskList.appendChild(task)
+
+	localStorage.setItem('tasks', finalTasks)
+
+	tasks.push(taskInput.value)
 }
 
 taskInput.addEventListener('keypress', (e) => {
